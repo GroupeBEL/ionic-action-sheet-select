7ec00794a0395c2965b538932c4cac9590bc465f
Frad <afrad_ext@groupe-bel.com>
2020-06-19 10:25:11 +0200

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  crns = [
+    { idct: 23, creneau: '11:45:00', nb_places: 61, nbre: 55, subs: 6 },
+    { idct: 27, creneau: '12:30:00', nb_places: 90, nbre: 90, subs: 0 },
+    { idct: 28, creneau: '13:15:00', nb_places: 120, nbre: 41, subs: 97 }
+  ];
+  form: FormGroup;
+  noCantine = 0;
+  constructor(
+    private formBuilder: FormBuilder
+  ) {
+    this.initForm();
+  }
 
-  constructor() {}
+  initForm() {
+    this.form = this.formBuilder.group({
+      canteen: [null]
+    });
+  }
+
+  changeTime() {
+
+  }
+
+  onCancel() {
+
+  }
+
+  getFormData() {
+
+  }
 
 }
